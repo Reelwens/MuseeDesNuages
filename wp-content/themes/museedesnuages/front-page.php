71f6f84f24d3560f876579fedf2e895e69c069a3
Simon LUCAS <sim.lucas27@gmail.com>
2018-03-17 01:28:47 +0100

    :bug: Fix parallax

--- wp-content/themes/museedesnuages/front-page.php
+++ wp-content/themes/museedesnuages/front-page.php
@@ -2,13 +2,13 @@
 
     <div id="front-page">
         <div class="hero">
-            <div class="texts" data-type="parallax" data-depth="-0.35">
+            <div class="texts" data-type="parallax" data-depth="-0.5">
                 <div class="title"><span class="second">Musée</span><span class="main">des nuages</span></div>
                 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto beatae dolore ipsa aliquid.</p>
             </div>
 
             <div class="parallax">
-                <div class="mountain layer" data-type="parallax" data-depth="0.1"></div>
+                <div class="mountain layer" data-type="parallax" data-depth="-0.2"></div>
                 <div class="cloud layer" data-type="parallax" data-depth="0.00"></div>
             </div>
         </div>
