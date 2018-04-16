f012e37ddab849aee8aef849a3296fa35ad78b19
Simon LUCAS <sim.lucas27@gmail.com>
2018-04-16 22:42:02 +0200

    :bug: Fix scroll bug

--- wp-content/themes/museedesnuages/src/js/visit.js
+++ wp-content/themes/museedesnuages/src/js/visit.js
@@ -5,23 +5,24 @@ on('/visit', arg => {
 
     // Scroll
     var lethargy = new Lethargy();
-    let scrollable = 11,
+    let scrollLimit = 11,
         count = 0,
         lastScroll = Date.now();
 
-    $(window).bind('mousewheel DOMMouseScroll wheel MozMousePixelScroll', function(e){
-        e.preventDefault()
+    $(window).bind('mousewheel DOMMouseScroll wheel MozMousePixelScroll', e => {
+        e.preventDefault();
         e.stopPropagation();
 
         if (Date.now() - lastScroll < 1200) return;
         lastScroll = Date.now();
 
-        if(lethargy.check(e) == 1 && count > 0) {
-            count--
+        let scrollValue = lethargy.check(e);
+
+        if (scrollValue === 1 && count > 0) {
+            count--;
             document.querySelector('body').dataset.scrollState = count;
-        }
-        else if (lethargy.check(e) == -1 && scrollable > count) {
-            count++
+        } else if (scrollValue === -1 && scrollLimit > count) {
+            count++;
             document.querySelector('body').dataset.scrollState = count;
         }
     });
@@ -35,8 +36,7 @@ on('/visit', arg => {
         if (e.keyCode == '38' && count > 0) {
             count--
             document.querySelector('body').dataset.scrollState = count;
-        }
-        else if (e.keyCode == '40' && scrollable > count) {
+        } else if (e.keyCode == '40' && scrollable > count) {
             count++
             document.querySelector('body').dataset.scrollState = count;
         }
