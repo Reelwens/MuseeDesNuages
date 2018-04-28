ad0261f08093565eca74fcdb3c7a0339a0e2c994
Simon LUCAS <sim.lucas27@gmail.com>
2018-04-29 00:33:30 +0200

    :sparkles: Parallax upgrade, logo, better transitions

--- wp-content/themes/museedesnuages/front-page.php
+++ wp-content/themes/museedesnuages/front-page.php
@@ -1,15 +1,16 @@
 <?php get_header();  ?>
 
     <div id="front-page">
+        <a href="/"><img src="<?= THEME_URL ?>/assets/img/logoWhite.png" alt="Logo" class="logoMDN" data-type="parallax" data-depth="0.5" /></a>
         <div class="hero" data-type="parallax" data-depth="0.00">
-            <div class="catchingText" data-type="parallax" data-depth="0.5">
+            <div class="catchingText" data-type="parallax" data-depth="0.6">
                 <div class="websiteTitle"><span class="second">musée</span><span class="main">des nuages</span></div>
                 <p class="resume">Un musée de notre siècle<br />où l'art inspire notre sensibilité environnementale</p>
             </div>
 
             <div class="parallax">
-                <div class="cloud_4 layer" data-type="parallax" data-depth="0.4"></div>
-                <div class="cloud_3 layer" data-type="parallax" data-depth="0.35"></div>
+                <div class="cloud_4 layer" data-type="parallax" data-depth="0.5"></div>
+                <div class="cloud_3 layer" data-type="parallax" data-depth="0.4"></div>
                 <div class="cloud_2 layer" data-type="parallax" data-depth="0.3"></div>
                 <div class="cloud_1 layer" data-type="parallax" data-depth="0.01"></div>
             </div>
