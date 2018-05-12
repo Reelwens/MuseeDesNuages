5839d30bcf6e66d757b818498c0c8daad718f766
Simon LUCAS <sim.lucas27@gmail.com>
2018-05-12 18:32:30 +0200

    :sparkles: Footer

--- wp-content/themes/museedesnuages/footer.php
+++ wp-content/themes/museedesnuages/footer.php
@@ -1,5 +1,18 @@
     <footer id="footer">
-        <p>Footer</p>
+        <div class="footer_left">
+            <img src="<?= THEME_URL ?>/assets/img/logoWhite.png" alt="Logo" class="logoFooter" />
+            <a href="tel:+33683853390" class="number">+33 (0)6 83 85 33 90</a>
+            <a href="mailto:museedesnuages@gmail.com" class="mail">museedesnuages@gmail.com</a>
+            <p class="copyright">©Copyright 2018</p>
+        </div>
+
+        <div class="footer_right">
+            <a href="<?= home_url() ?>/" class="navLink">Accueil</a>
+            <a href="<?= home_url() ?>/visit" class="navLink">Visite</a>
+            <a href="<?= home_url() ?>/news" class="navLink">Actualités</a>
+            <a href="<?= home_url() ?>/services" class="navLink">Prestations</a>
+            <a href="<?= home_url() ?>/contact" class="navLink">Contact</a>
+        </div>
     </footer>
 
     <div class="extra">
