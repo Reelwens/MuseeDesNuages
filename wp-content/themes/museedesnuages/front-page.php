cb784ee6ef38d93557a6cd7dba3e1bcc1118060d
Simon LUCAS <sim.lucas27@gmail.com>
2018-05-12 15:06:58 +0200

    :sparkles: ACF home fields

--- wp-content/themes/museedesnuages/front-page.php
+++ wp-content/themes/museedesnuages/front-page.php
@@ -1,15 +1,14 @@
 <?php get_header();  ?>
-
     <div id="front-page">
         <a href="/"><img src="<?= THEME_URL ?>/assets/img/logoWhite.png" alt="Logo" class="logoMDN" data-type="parallax" data-depth="0.5" /></a>
         <div class="hero" data-type="parallax" data-depth="0.00">
             <div class="catchingText" data-type="parallax" data-depth="0.6">
                 <div class="websiteTitle"><span class="second">musée</span><span class="main">des nuages</span></div>
-                <p class="resume">Un musée de notre siècle<br />où l'art inspire notre sensibilité environnementale</p>
+                <p class="resume"><?php the_field('resume_sous-titre'); ?></p>
             </div>
 
             <div class="scrollInvitation" data-type="parallax" data-depth="0.6">
-                <p>défiler vers le bas<span class="hiddenMobile"> pour plonger dans les nuages</span></p>
+                <p><?php the_field('scroll'); ?></p>
                 <div class="lineScroll"><img src="<?= THEME_URL ?>/assets/img/line_scroll.svg" alt="Line scroll"></div>
             </div>
 
@@ -92,84 +91,83 @@
             <div id="triggerGradient"></div>
 
             <div class="heading">
-                <p class="second">Un musée sans cimaise, sans mur et sans frontières.</p>
-                <p class="first">Suivez les nuages, respirez l’air du temps, nous vous accompagnons.</p>
+                <p class="second"><?php the_field('tete_de_texte_1'); ?></p>
+                <p class="first"><?php the_field('tete_de_texte_2'); ?></p>
             </div>
 
             <div class="point point-1"></div>
             <p class="paragraph paragraph-1">
-                <span class="important">En 1988, depuis l’espace clos d’une salle d’exposition,</span> Sylvain Soussan regarde par la fenêtre ; il observe le monde extérieur et décide d’associer patrimoine culturel et patrimoine naturel.
+                <?php the_field('paragraphe_1'); ?>
             </p>
             <div class="point point-2"></div>
             <p class="paragraph paragraph-2">
-                Regardant par la fenêtre d’un musée, il observe une nature progressivement contrainte et surveillée, tel un objet de collection. <span class="important">La beauté du monde lui apparait alors aussi fragile que celle des œuvres d’art.</span>
+                <?php the_field('paragraphe_2'); ?>
             </p>
             <div class="point point-3"></div>
             <p class="paragraph paragraph-3">
-                Retournant vers la vitrine qu’il avait quittée des yeux, il constate que la valeur d’une collection est déterminée par la rareté des pièces qu’elle abrite, alors qu’on apprécie les biens naturels pour leur abondance. <span class="important">Les biens naturels ne sont pas pour autant moins précieux.</span>
+                <?php the_field('paragraphe_3'); ?>
             </p>
             <div class="point point-4"></div>
             <p class="paragraph paragraph-4">
-                <span class="important">Ainsi sera conçu le musée des nuages qui nous invite à explorer notre environnement autant que notre sensibilité.</span> Partir dans les nuages, c’est aussi rêver et s’affranchir des cadres, c’est laisser s’emballer notre imagination ; le musée des nuages sera un musée sans mur et ouvert au partage.
+                <?php the_field('paragraphe_4'); ?>
             </p>
             <div class="point point-5"><img src="<?= THEME_URL ?>/assets/img/art-icon.svg" alt="Art" /></div>
-            <p class="paragraph paragraph-5">art</p>
+            <p class="paragraph paragraph-5"><?php the_field('paragraphe_5'); ?></p>
             <div class="point point-6"></div>
             <p class="paragraph paragraph-6">
-                Depuis le XXe siècle, l’histoire de l’art démontre que nous sommes potentiellement tous des artistes. Artistes sans le savoir, nous sommes aussi vandales sans le vouloir, envers une nature qu’il faudrait soigner comme si elle était notre création.
+                <?php the_field('paragraphe_6'); ?>
             </p>
             <div class="point point-7"></div>
             <p class="paragraph paragraph-7">
-                Être dans les nuages c’est rêver. Le musée des nuages rêve d’un art écologique auquel tout participe. Que chaque chose y trouve sa place : les œuvres de musées et les œuvres privées, les trésors de l’instant et les joyaux éternels ; que l’art entre dans la chaine alimentaire de toutes les espèces ; qu’il survole les barrières ; qu’il habite les rêves des enfants comme ceux des nations.
+                <?php the_field('paragraphe_7'); ?>
             </p>
             <div class="point point-8"></div>
             <p class="paragraph paragraph-8">
-                Le regard des artistes modifie notre vision du monde et nous aide à mieux percevoir notre environnement. Nous croisons leurs points de vues avec ceux des publics et des scientifiques afin d’imaginer ensemble une collection pour notre temps.
+                <?php the_field('paragraphe_8'); ?>
             </p>
             <div class="point point-9"><img src="<?= THEME_URL ?>/assets/img/climat-icon.svg" alt="Climat" /></div>
-            <p class="paragraph paragraph-9">climat</p>
+            <p class="paragraph paragraph-9"><?php the_field('paragraphe_9'); ?></p>
             <div class="point point-10"></div>
             <p class="paragraph paragraph-10">
-                <span class="important">96 % des scientifiques diagnostiquent un réchauffement de l’atmosphère d’origine humaine.</span> Suffirait-il alors que l’humain cesse ses activités néfastes pour que tout rentre dans l’ordre ?
+                <?php the_field('paragraphe_10'); ?>
             </p>
             <div class="point point-11"></div>
             <p class="paragraph paragraph-11">
-                Sur plus de <span class="big">80 000 essences</span> existantes<br />moins de <span class="big">1 % ont été étudiées</span> aux fins de leur utilisation
+                <?php the_field('paragraphe_11'); ?>
             </p>
             <div class="point point-12"></div>
             <p class="paragraph paragraph-12">
-                Entre <span class="big">1990 et 2015</span><br />
-                la proportion de la population mondiale ayant connu une amélioration de sa source <span class="big">d’eau potable a augmenté de 76 % à 91 %</span><br />ce qui représente <span class="big">2,6 milliards de personnes</span>
+                <?php the_field('paragraphe_12'); ?>
             </p>
             <div class="point point-13"></div>
             <p class="paragraph paragraph-13">
-                <span class="important">L’art pour stimuler la sensibilité environnementale.</span><br />Le musée des nuages propose l’art pour stimuler la sensibilité environnementale afin que tous : individus, collectivités, entreprises, nous puissions adopter des comportements naturels, adaptés à nos situations et en accord avec les enjeux de notre temps.
+                <?php the_field('paragraphe_13'); ?>
             </p>
             <div class="point point-14"></div>
             <p class="paragraph paragraph-14">
-                <span class="important">Donner du sens l’effort environnemental.</span> Notre ingénierie culturelle propose des supports de communication pour accompagner les démarches, mettre en valeur les efforts accomplis et porter à la lumière les résultats obtenus.
+                <?php the_field('paragraphe_14'); ?>
             </p>
             <div class="point point-15"><img src="<?= THEME_URL ?>/assets/img/partage-icon.svg" alt="Partage" /></div>
-            <p class="paragraph paragraph-15">partage</p>
+            <p class="paragraph paragraph-15"><?php the_field('paragraphe_15'); ?></p>
             <div class="point point-16"></div>
             <p class="paragraph paragraph-16">
-                <span class="important">Au plaisir de découvrir des œuvres d’art, ajoutons celui de savoir.</span> Pour engendrer des connaissances, nous devons faire œuvre de pédagogie, de partage et de restitution
+                <?php the_field('paragraphe_16'); ?>
             </p>
             <div class="point point-17"></div>
             <p class="paragraph paragraph-17">
-                <span class="important">Un art participe, pour une science participative : la météorologie.</span> Parmi les sciences naturalistes, dont on s’accorde à dire qu’elles furent les précurseurs des sciences participatives, la météorologie n’aurait pas pu se développer sans la contribution d’un réseau d’observateurs. Sur le terrain du climat, l’art est plus que jamais attentif à la participation, à la co-construction et à toutes formes d’interactions.
+                <?php the_field('paragraphe_17'); ?>
             </p>
             <div class="point point-18"></div>
             <p class="paragraph paragraph-18">
-                <span class="important">Des collections de rêves.</span> Nos collections sont accrochées à des cimaises de rêves.<br />En partageant un morceau de paysage en demandant à un artiste d’installer une œuvre sur un site que vous choisissez, c’est vous qui agrandissez nos réserves. Ainsi, la visite de ne se limite pas aux murs d’une galerie, ne se concentre pas en un seul endroit, mais se parcourt à l’échelle de territoires reliés par une même grille de lecture, celle qui suit le fil de l’eau et déchiffre les signes dans le ciel.
+                <?php the_field('paragraphe_18'); ?>
             </p>
             <div class="point point-19"></div>
             <p class="paragraph paragraph-19">
-                <span class="important">Nuage d’objets, première collection matérielle.</span><br />À la suite de l’inscription « musée des nuages » sur le château d’eau de Poitiers-Bellejouanne, le musée a réuni une collection d’objets prêtés. Chaque objet a été numéroté, classé, estampillé puis exposé. À la fin de l’exposition, le nuage d’objets a été dispersé et les prêts restitués. En récupérant leur bien, les prêteurs sont devenus conservateurs d’un objet appartenant à un musée qui leur appartient.
+                <?php the_field('paragraphe_19'); ?>
             </p>
             <div class="point point-20"></div>
             <p class="paragraph paragraph-20">
-                Autour des œuvres d’art et des moyens que nous développons, le musée des nuages valorise vos ressources, propose des expériences et favorise les échanges.
+                <?php the_field('paragraphe_20'); ?>
             </p>
             <div class="point point-21"></div>
             <div class="paragraph paragraph-21">
