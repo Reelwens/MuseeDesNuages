6618276831eb9cb32c890788fde982e5cff1770e
Simon LUCAS <sim.lucas27@gmail.com>
2018-05-15 00:26:55 +0200

    :sparkles: Upgrade footer

--- wp-content/themes/museedesnuages/footer.php
+++ wp-content/themes/museedesnuages/footer.php
@@ -3,7 +3,11 @@
             <img src="<?= THEME_URL ?>/assets/img/logoWhite.png" alt="Logo" class="logoFooter" />
             <a href="tel:+33683853390" class="number">+33 (0)6 83 85 33 90</a>
             <a href="mailto:museedesnuages@gmail.com" class="mail">museedesnuages@gmail.com</a>
-            <p class="copyright">©Copyright 2018</p>
+            <p class="newsletterTitle">Newsletter</p>
+            <div class="newsletterInput">
+                <input type="text" placeholder="Email" />
+                <img src="<?= THEME_URL ?>/assets/img/transfer.svg" alt="Arrow" class="arrow" />
+            </div>
         </div>
 
         <div class="footer_right">
@@ -12,7 +16,15 @@
             <a href="<?= home_url() ?>/news" class="navLink">Actualités</a>
             <a href="<?= home_url() ?>/services" class="navLink">Prestations</a>
             <a href="<?= home_url() ?>/contact" class="navLink">Contact</a>
+
+            <div class="social">
+                <a href="#"><img src="<?= THEME_URL ?>/assets/img/Facebook.svg" alt="Facebook" class="facebook" /></a>
+                <a href="#"><img src="<?= THEME_URL ?>/assets/img/Twitter.svg" alt="Twitter" class="twitter" /></a>
+                <a href="#"><img src="<?= THEME_URL ?>/assets/img/Instagram.svg" alt="Instagram" class="instagram" /></a>
+                <a href="#"><img src="<?= THEME_URL ?>/assets/img/Linkedin.svg" alt="Linkedin" class="linkedin" /></a>
+            </div>
         </div>
+        <p class="copyright">©Copyright 2018</p>
     </footer>
 
     <div class="extra">
