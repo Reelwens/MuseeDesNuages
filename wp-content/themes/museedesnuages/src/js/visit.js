692065e59a21b93e1dc789c4e29da8df5ef09d4b
Simon LUCAS <sim.lucas27@gmail.com>
2018-05-23 20:36:42 +0200

    :sparkles: Fix scroll mac & add arrow feature

--- wp-content/themes/museedesnuages/src/js/visit.js
+++ wp-content/themes/museedesnuages/src/js/visit.js
@@ -2,13 +2,20 @@ on('/visit', arg => {
     /*
      * Set data scroll state
      */
+
+    // Scroll
     var lethargy = new Lethargy();
     let scrollable = 10,
-        count = 0;
+        count = 0,
+        lastScroll = Date.now();
 
     $(window).bind('mousewheel DOMMouseScroll wheel MozMousePixelScroll', function(e){
         e.preventDefault()
         e.stopPropagation();
+
+        if (Date.now() - lastScroll < 1200) return;
+        lastScroll = Date.now();
+
         if(lethargy.check(e) == 1 && count > 0) {
             count--
             document.querySelector('body').dataset.scrollState = count;
@@ -18,6 +25,22 @@ on('/visit', arg => {
             document.querySelector('body').dataset.scrollState = count;
         }
     });
+
+    // Arrow
+    document.onkeydown = checkKey;
+
+    function checkKey(e) {
+        e = e || window.event;
+
+        if (e.keyCode == '38') {
+            count--
+            document.querySelector('body').dataset.scrollState = count;
+        }
+        else if (e.keyCode == '40') {
+            count++
+            document.querySelector('body').dataset.scrollState = count;
+        }
+    }
 
 
 
