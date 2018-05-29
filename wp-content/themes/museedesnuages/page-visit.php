d52c5169a26f0492c116ac29711cefefca35c432
Simon LUCAS <sim.lucas27@gmail.com>
2018-05-29 16:03:09 +0200

    :sparkles: Rewrite animations

--- wp-content/themes/museedesnuages/page-visit.php
+++ wp-content/themes/museedesnuages/page-visit.php
@@ -32,45 +32,55 @@
             <div class="subFrame subFrame-2">
                 <h2 class="subFrameName">Châteaux d'eau</h2>
                 <p class="description">Un château d’eau est un repère, un signe de prospérité, un témoin de civilisation. Intégrer un château d’eau dans les collections du musée des nuages c’est « restaurer » le regard des habitants sur leurs équipements urbains.</p>
+                <a href="#" class="buttonDetail">Voir le détail du projet</a>
+            </div>
+            <div class="subFrameImg subFrame-2">
                 <img src="<?= THEME_URL ?>/assets/img/castle1.png" alt="Chateau" class="card card-1" />
                 <img src="<?= THEME_URL ?>/assets/img/castle2.png" alt="Chateau" class="card card-2" />
                 <img src="<?= THEME_URL ?>/assets/img/castle3.png" alt="Chateau" class="card card-3" />
-                <a href="#" class="buttonDetail">Voir le détail du projet</a>
             </div>
 
             <div class="subFrame subFrame-3">
                 <h2 class="subFrameName">Bouteilles</h2>
                 <p class="description">Bouteilles et verres à eau estampillés musée des nuages sont des petits monuments individuels dédiés à la conscience de l’eau et à son partage.</p>
+                <a href="#" class="buttonDetail">Voir le détail du projet</a>
+            </div>
+            <div class="subFrameImg subFrame-3">
                 <img src="<?= THEME_URL ?>/assets/img/bottle-1.png" alt="Bouteille" class="card card-1" />
                 <img src="<?= THEME_URL ?>/assets/img/bottle-2.png" alt="Bouteille" class="card card-2" />
                 <img src="<?= THEME_URL ?>/assets/img/bottle-3.png" alt="Bouteille" class="card card-3" />
-                <a href="#" class="buttonDetail">Voir le détail du projet</a>
             </div>
 
             <div class="subFrame subFrame-4">
                 <h2 class="subFrameName">Fontaines</h2>
                 <p class="description">Autour des puits, des fontaines, des lavoirs ou d’un verre en terrasse, l’eau est un facteur de convivialité. Du monumental ouvrage d’art conçu pour l’irrigation, au robinet de confort qui pénétra dans l’intimité des habitats, cet élément vital fut de tout temps un vecteur de culture.</p>
+                <a href="#" class="buttonDetail">Voir le détail du projet</a>
+            </div>
+            <div class="subFrameImg subFrame-4">
                 <img src="<?= THEME_URL ?>/assets/img/fountain-1.png" alt="Fontaine" class="card card-1" />
                 <img src="<?= THEME_URL ?>/assets/img/fountain-2.png" alt="Fontaine" class="card card-2" />
                 <img src="<?= THEME_URL ?>/assets/img/fountain-3.png" alt="Fontaine" class="card card-3" />
-                <a href="#" class="buttonDetail">Voir le détail du projet</a>
             </div>
 
             <div class="subFrame subFrame-5">
                 <h2 class="subFrameName">Cocktail 5 sources</h2>
                 <p class="description">Le cocktail 5 sources est un assemblage de quatre eaux de source, plus une : l’eau de robinet ! </p>
+                <a href="#" class="buttonDetail">Voir le détail du projet</a>
+            </div>
+            <div class="subFrameImg subFrame-5">
                 <img src="<?= THEME_URL ?>/assets/img/cocktail1.png" alt="Cocktail" class="card card-1" />
                 <img src="<?= THEME_URL ?>/assets/img/cocktail2.png" alt="Cocktail" class="card card-2" />
                 <img src="<?= THEME_URL ?>/assets/img/cocktail3.png" alt="Cocktail" class="card card-3" />
-                <a href="#" class="buttonDetail">Voir le détail du projet</a>
             </div>
 
             <div class="subFrame subFrame-6">
                 <h2 class="subFrameName">Piscine des sources</h2>
                 <p class="description">Une piscine dans laquelleon peut se servir à boire en plongeant les mains dans le bassin, pour saisir des bouteilles d’eau à consommer sur place ou plus tard.</p>
+                <a href="#" class="buttonDetail">Voir le détail du projet</a>
+            </div>
+            <div class="subFrameImg subFrame-6">
                 <img src="<?= THEME_URL ?>/assets/img/pool1.png" alt="Piscine" class="card card-1" />
                 <img src="<?= THEME_URL ?>/assets/img/pool2.png" alt="Piscine" class="card card-2" />
-                <a href="#" class="buttonDetail">Voir le détail du projet</a>
             </div>
 
 
