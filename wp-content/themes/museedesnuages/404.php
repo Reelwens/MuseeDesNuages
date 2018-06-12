67939e784cb23e196eacf67b0fce77e52b780eb0
lisafises <lisa.fises@hetic.net>
2018-06-12 22:44:58 +0200

    :lipstick: contact responsive

--- wp-content/themes/museedesnuages/404.php
+++ wp-content/themes/museedesnuages/404.php
@@ -1,2 +1,9 @@
-<h1>404</h1>
+<?php
+get_header();
+?>
+<div class="error__page">
+    <div class="headerPage">
+        <h1>Page non trouvée</h1>
+    </div>
+</div>
 <a href="<?= home_url() ?>">Retour à l'acceuil</a>
