ee12a87f1fba4d2946fc7013f4ac033b4495e0bf
lisafises <lisa.fises@hetic.net>
2018-06-12 23:46:47 +0200

    :lipstick: add 404 page

--- wp-content/themes/museedesnuages/404.php
+++ wp-content/themes/museedesnuages/404.php
@@ -3,7 +3,11 @@ get_header();
 ?>
 <div class="error__page">
     <div class="headerPage">
-        <h1>Page non trouvée</h1>
+        <h1 class="headerTitle">Page non trouvée</h1>
+        <p class="headerSubtitle">Désolée, la page demandée n'existe pas dans le musée des nuages...</p>
+        <a href="<?= home_url() ?>" class="button">
+            <span>Retour à l'accueil</span>
+            <img src="<?= THEME_URL ?>/assets/img/arrow_right.svg" alt="Flèche" />
+        </a>
     </div>
 </div>
-<a href="<?= home_url() ?>">Retour à l'acceuil</a>
