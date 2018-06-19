fec7455541223fc87671f59c79e07728688c50c3
lisafises <lisa.fises@hetic.net>
2018-06-20 01:18:58 +0200

    :construction: articles sorting by tag

--- wp-content/themes/museedesnuages/page-news.php
+++ wp-content/themes/museedesnuages/page-news.php
@@ -14,13 +14,49 @@ get_header();
             <h1 class="headerTitle">Nos actualités</h1>
             <p class="headerSubtitle">Lorem Ipsum Dolores Dolores Dolores Dolores</p>
         </div>
-
-        <!-- Add anchor here -->
+        <div class="headerTerms">
+            <ul class="subMenu">
+                <li><a href="#">articles</a></li>
+                <li><a href="#">réseaux sociaux</a></li>
+            </ul>
+            <ul class="termsLinks">
+                <li class="termLink"><a href="#">voir tout</a></li>
+                <?php if ($terms = get_terms('theme', 'orderbyname')):
+                 foreach ($terms as $term): ?>
+                    <li class="termLink"><a href="#"><?php echo $term->name; ?></a></li>
+                <?php endforeach;
+                endif;
+                ?>
+            </ul>
+        </div>
 
         <div class="listArticles">
         <?php
-        $paged = ( get_query_var('paged') ) ? get_query_var('paged') : 1;
-        $loop = new WP_Query( array( 'post_type' => 'news', 'posts_per_page' => 7, 'paged' => $paged ) ); ?>
+        $paged = (get_query_var('paged')) ? get_query_var('paged') : 1;
+
+        $category = (get_query_var('category'));
+
+        $loop = new WP_Query( array(
+            'post_type' => 'news',
+            'posts_per_page' => 7,
+            'order' => 'DESC',
+            'paged' => $paged )
+        );
+
+        // Displays article terms with commas
+        function display_terms() {
+            $terms = get_the_terms( get_the_ID() , 'theme' );
+            $multiple_terms = '';
+            if ($terms) {
+                foreach ( $terms as $term ) {
+                    $multiple_terms .= $term->name . ', ';
+                }
+                $multiple_terms = rtrim( $multiple_terms, ', ' );
+                echo $multiple_terms;
+            }
+        }
+
+        ?>
 
         <?php while ( $loop->have_posts() ) : $loop->the_post(); ?>
 
@@ -29,7 +65,7 @@ get_header();
                 <div class="gradient"></div>
                 <div class="content">
                     <div class="info">
-                        <p class="theme">Air</p> <!-- Add tag function here -->
+                        <p class="theme"><?php echo display_terms(); ?></p>
                         <p class="date"><?= get_the_date() ?></p>
                     </div>
                     <div class="articleTitle">
